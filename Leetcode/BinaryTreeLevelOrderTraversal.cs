6dfa1908fcc4630e9e118ff1f7560b2cce3cf79f
Alexey Smirnov <bezkalitnikov@gmail.com>
2021-10-23 10:27:22 +0300

    Initial commit

---
+++ Leetcode/BinaryTreeLevelOrderTraversal.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace leetcode
+{
+    public class BinaryTreeLevelOrderTraversal
+    {
+    public IList<IList<int>> LevelOrder(TreeNode root) 
+    {
+        
+    }
+
+    public IList<IList<int>> LevelOrder(IList<IList<int>> root, IList<IList<int>> current, TreeNode node)
+    {
+
+    }
+    }
+}
